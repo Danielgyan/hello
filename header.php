2e22c37ff4cc36d14af3b53040217758a020c063
Danielgyan <daniel.grigoryan.2004@bk.ru>
2020-10-10 22:09:56 +0400

    change header page

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 
 <header>
 <h1 style="background=color: blue;">header</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt facilis dolores, recusandae exercitationem nam temporibus itaque iure incidunt quos aliquam minima quia sequi eveniet odit adipisci neque! Est, dolores modi!</p>
 </header>
